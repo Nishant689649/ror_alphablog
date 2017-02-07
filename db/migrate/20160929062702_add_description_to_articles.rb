77d214ebcbed6612e6196522a5114b5a227e385d
Nishant <nishantdeshmukh71@gmail.com>
2017-02-07 18:54:58 +0000

    testing application

---
+++ db/migrate/20160929062702_add_description_to_articles.rb
@@ -0,0 +1,9 @@
+class AddDescriptionToArticles < ActiveRecord::Migration
+  def change
+  add_column :articles, :description, :text_field
+  add_column :articles, :created_at, :datetime
+  add_column :articles, :updated_at, :datetime 
+  
+  
+  end
+end
